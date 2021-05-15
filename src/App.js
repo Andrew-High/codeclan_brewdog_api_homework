0e3a438f59ba6b4ad998e3a36af6e7be246ebef7
Andrew High <andrewhigh4@hotmail.com>
2021-05-15 09:20:37 +0100

    added css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import BeerContainer from './containers/BeerContainer';
 
 function App() {
   return (
-    <div className="App">
+    <div id="app">
       <div id="header">
         <img src= "https://cdn.shortpixel.ai/spai/w_300+q_lossy+ret_img+to_webp/https://mk0hive9wr7agvd1g6.kinstacdn.com/wp-content/uploads/2019/08/Brewdog-Black-Horizontal.png" width = "200" alt=""/>
         <h1 id="header-text">by Numbers</h1>
